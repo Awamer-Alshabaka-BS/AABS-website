c6960ff64c4ecc543fb02690e4fd0ffffc7ed1d0
AwamerAlshabaka <111978652+AwamerAlshabaka@users.noreply.github.com>
2024-06-13 15:26:19 +0300

    .

---
+++ aait_website_style/__manifest__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "AAIT Website Style",
+
+    'summary': """
+        AABS Website Navbar and Footer Style from AAIT  main Website
+        """,
+
+    'description': """
+        AABS Website Navbar and Footer Style from AAIT  main Website
+    """,
+
+    'author': "AAIT , Mohamed Reda , Mohamed Abuelmagd",
+    # 'editor':"Mohamed Abuelmagd"
+    'website': "https://aait.sa",
+
+    
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base','web','website'],
+
+    'data': [
+        'views/navbar.xml',
+        'views/footer.xml',
+        # 'views/whatsappview.xml',
+    ],
+    'assets': {
+       'web.assets_frontend': [
+        #    'aait_website_style/static/src/css/aait_style.css',
+        # 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css',
+        # 'aait_website_style/static/src/css/whatsapp-float.csss',
+           'aait_website_style/static/src/css/style.css',
+        #    'aait_website_style/static/src/css/index-style.css',
+        #    'aait_website_style/static/src/css/bootstrap.min.css',
+        #    'aait_website_style/static/src/css/scss/_dir-mixins.scss',
+        #    'aait_website_style/static/src/css/scss/_variables.scss',
+        #    'aait_website_style/static/src/css/scss/style.scss',
+        #    'aait_website_style/static/src/css/scss/_classes.scss',
+           'aait_website_style/static/src/css/font-awesome-5all.css',
+        #    'aait_website_style/static/src/css/scss/_settings.scss',
+           
+            ],
+    },
+}
